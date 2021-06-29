35ace2678c683ed18b0515621bd011294b18ab38
Otto Vintola <otto.e.vintola@gmail.com>
2021-06-29 13:54:30 +0300

    lexi

--- ch.cpp
+++ ch.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <locale>
+#include <string>
+
 
 int main () {
 
-    std::cout<<"aaa";
+    std::string n;
+    std::cin>>n;
+
+      std::locale loc;
+
+      for(std::string::size_type  i = 0; i < n.length(); ++i) {
+
+
+        std::cout<<std::tolower(n[i], loc);
+        std::cout<<"Oaaa";
+      }
+
+
 
 }
